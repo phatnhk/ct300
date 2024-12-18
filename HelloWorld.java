a6bf1277654fe6c3a34361f2cd3c30f9b9edc195
PhatNHK <phat.nhk@tbd.edu.vn>
2024-12-18 23:02:57 +0700

    Them noi dung cho nhanh hotfix

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Đây là nơi bạn viết mã cần thực thi
         System.out.println("Chào mừng bạn đến với Java!");
+        System.out.println("============================");
+        System.out.println("Toi vua tao them nhanh HotFix");
     }
 }
