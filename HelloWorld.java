5f1cdb2761eaaecaada85b2655e8a85aeec9d709
PhatNHK <phat.nhk@tbd.edu.vn>
2024-12-18 21:55:04 +0700

    Them file HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Đây là nơi bạn viết mã cần thực thi
+        System.out.println("Chào mừng bạn đến với Java!");
+    }
+}
